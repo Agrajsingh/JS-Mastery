3da712efc98e776ab1b9cd41a983f8db7f10aecf
Agrajsingh <143416360+Agrajsingh@users.noreply.github.com>
2026-04-08 17:54:30 +0530

    Add whole JS Mastery files

---
+++ Phase-1/code.js
@@ -0,0 +1,39 @@
+
+/*---------> if-else, loop, switch<------------*/
+
+
+/*function linearSearch(arr, target){
+  for(let i = 0; i<arr.length;i++){
+    if(arr[i]==target){
+        return i
+    }
+
+  }
+  return -1
+
+}
+
+let arr = [12,4,5,2,9,3]
+let target = 3;
+console.log(linearSearch(arr, target)) */
+
+/*    Binary search
+function binarySearch(array,target){
+    let left = 0;
+    let right = array.length-1;
+    while(left <= right){
+        let mid = (left+right)/2
+        if(array[mid]== target) {
+            return mid;
+        }else if(array[mid] < target){
+            left = mid+1;
+        }else{
+            right = mid-1
+        }
+
+        }
+        return -1;
+    }
+*/
+
+
